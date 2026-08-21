32d4079dcd1592583db5b666ca6757d6a56f0261
karl2603 <arvindrajkarl@gmail.com>
2026-08-21 17:06:17 +0530

    Entities

--- src/main/java/com/Karl/SecureDesk/entity/Ticket.java
+++ src/main/java/com/Karl/SecureDesk/entity/Ticket.java
@@ -1,4 +1,31 @@
 package com.Karl.SecureDesk.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tickets")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long t_id;
+    private String title;
+    private String description;
+    private String status;
+    private LocalDateTime createdAt;
+    private LocalDateTime lastUpdatedAt;
+    @JsonBackReference
+    @ManyToOne
+    @JoinColumn(name = "u_id")
+    private User user;
 }
--- src/main/java/com/Karl/SecureDesk/entity/User.java
+++ src/main/java/com/Karl/SecureDesk/entity/User.java
@@ -1,4 +1,31 @@
 package com.Karl.SecureDesk.entity;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long u_id;
+    private String username;
+    private String email;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
+    @JsonManagedReference
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "user")
+    private List<Ticket> tickets;
 }
