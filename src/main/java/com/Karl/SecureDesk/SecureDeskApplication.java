0011af58dab07a5f3bd56211133599421c9f79e9
karl2603 <arvindrajkarl@gmail.com>
2026-08-21 16:27:41 +0530

    SecureDesk

---
+++ src/main/java/com/Karl/SecureDesk/SecureDeskApplication.java
@@ -0,0 +1,13 @@
+package com.Karl.SecureDesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Karl/SecureDesk/SecureDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Karl.SecureDesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
