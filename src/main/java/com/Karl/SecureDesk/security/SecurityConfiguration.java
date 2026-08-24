961fc7be36d3acb9f5f260fe83db30d963c703d0
karl2603 <arvindrajkarl@gmail.com>
2026-08-24 15:00:01 +0530

    Custom Security Filter Chain

--- src/main/java/com/Karl/SecureDesk/security/SecurityConfiguration.java
+++ src/main/java/com/Karl/SecureDesk/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,4 +18,24 @@ public class SecurityConfiguration {
         return provider;
     }
 
+    // Security Filter Chain
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            AuthenticationProvider authenticationProvider) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/SecureDesk/home",
+                                "/SecureDesk/users/register"
+                        ).permitAll()
+                        .anyRequest().hasAnyRole("USER", "ADMIN")
+                )
+                .authenticationProvider(authenticationProvider);
+
+        return http.build();
+    }
 }
+
