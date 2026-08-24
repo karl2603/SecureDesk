7558ef64844f6e239fa915b96545a37f37f28f99
karl2603 <arvindrajkarl@gmail.com>
2026-08-24 14:50:52 +0530

    Creating new tickets feature

--- src/main/java/com/Karl/SecureDesk/controller/TicketController.java
+++ src/main/java/com/Karl/SecureDesk/controller/TicketController.java
@@ -1,8 +1,25 @@
 package com.Karl.SecureDesk.controller;
 
+import com.Karl.SecureDesk.dto.CreateTicketRequest;
+import com.Karl.SecureDesk.service.TicketService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/SecureDesk")
 public class TicketController {
+    @Autowired
+    private TicketService ticketService;
 
+    @PostMapping("/ticket")
+    public ResponseEntity<CreateTicketRequest> createTicket(@Valid @RequestBody CreateTicketRequest ticketRequest){
+        ticketService.createTicket(ticketRequest);
+        return new ResponseEntity<>(ticketRequest, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/Karl/SecureDesk/service/TicketService.java
+++ src/main/java/com/Karl/SecureDesk/service/TicketService.java
@@ -1,4 +1,25 @@
 package com.Karl.SecureDesk.service;
 
+import com.Karl.SecureDesk.dto.CreateTicketRequest;
+import com.Karl.SecureDesk.entity.Ticket;
+import com.Karl.SecureDesk.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
 public class TicketService {
+    @Autowired
+    private TicketRepository ticketRepository;
+
+    public void createTicket(CreateTicketRequest ticketRequest){
+        Ticket ticket = new Ticket();
+        ticket.setTitle(ticketRequest.getTitle());
+        ticket.setDescription(ticketRequest.getDescription());
+        ticket.setStatus("ACTIVE");
+        ticket.setCreatedAt(LocalDateTime.now());
+        ticket.setLastUpdatedAt(LocalDateTime.now());
+        ticketRepository.save(ticket);
+    }
 }
